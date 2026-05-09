06d5d3128029666b3ba54f284980c67f5c08e12e
Sofia Manzhosova <sonf43@mail.ru>
2026-05-09 10:30:49 +0300

    Саммари

--- BL/NailWarehouse.Services/ProductServiceLogWrapper.cs
+++ BL/NailWarehouse.Services/ProductServiceLogWrapper.cs
@@ -5,18 +5,27 @@ using NailWarehouse.Services.Contracts;
 
 namespace NailWarehouse.Services;
 
+/// <summary>
+/// Декоратор для логирования производительности методов ProductService
+/// </summary>
 public class ProductServiceLogWrapper : IProductService
 
 {
     private readonly IProductService innerServiceLog;
     private readonly ILogger<ProductServiceLogWrapper> loggerLog;
 
+    /// <summary>
+    /// Инициализирует декоратор, принимая основной сервис и логгер для записи аналитики
+    /// </summary>
     public ProductServiceLogWrapper(IProductService innerService, ILogger<ProductServiceLogWrapper> logger)
     {
         innerServiceLog = innerService;
         loggerLog = logger;
     }
 
+    /// <summary>
+    /// Добавляет товар в систему с замером времени выполнения операции
+    /// </summary>
     public void Add(Product product)
     {
         var sw = Stopwatch.StartNew();
@@ -31,6 +40,9 @@ public class ProductServiceLogWrapper : IProductService
         }
     }
 
+    /// <summary>
+    /// Удаляет товар из системы с замером времени выполнения операции
+    /// </summary>
     public void Remove(Product product)
     {
         var sw = Stopwatch.StartNew();
@@ -45,6 +57,9 @@ public class ProductServiceLogWrapper : IProductService
         }
     }
 
+    /// <summary>
+    /// Возвращает список всех товаров с замером времени выполнения операции
+    /// </summary>
     public List<Product> GetAll()
     {
         var sw = Stopwatch.StartNew();
@@ -55,10 +70,13 @@ public class ProductServiceLogWrapper : IProductService
         finally
         {
             sw.Stop();
-            loggerLog.LogInformation("Метод GetAll выполнен за {Exapsed} мс", sw.ElapsedMilliseconds);
+            loggerLog.LogInformation("Метод GetAll выполнен за {Elapsed} мс", sw.ElapsedMilliseconds);
         }
     }
 
+    /// <summary>
+    /// Получает аналитику по складу (статистику) с замером времени выполнения операции
+    /// </summary>
     public (int count, decimal avgPrice, int totalQty, Product? deficit) GetStats()
     {
         var sw = Stopwatch.StartNew();
